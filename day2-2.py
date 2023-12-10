8c203a3847c5ca132f578bdd2290b255a36c6e3c
Kent <mourao@pt.lu>
2023-12-10 01:10:09 +0100

    Day2 Part2

---
+++ day2-2.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+f = open("day2-input.txt", "rt").read().strip()
+
+DIC_max = {'red': 12, 'blue': 14, 'green': 13}
+
+answer = 0
+answer2 = 0
+for line in f.splitlines():
+    valid = True
+    idf, line = line.split(":")
+    DIC = defaultdict(int)
+    for tirage in line.split(";"):
+        for boules in tirage.split(","):
+            nbr, couleur = boules.split()
+            if int(nbr) > DIC_max[couleur]:
+                valid = False
+            if DIC[couleur] < int(nbr):
+                DIC[couleur] = int(nbr)
+                
+    score = DIC['red'] * DIC['green'] * DIC['blue']
+    answer2 += score
+    if valid:
+        answer += int(idf.split()[1])
+                
+print(answer)
+print(answer2)
